2e7a7fe9270690a789db46b984c0b34169c0bfd8
Minami Takuto <25cr05tm@ismxd2.head.xd-local.ism.ac.jp>
2025-08-01 00:54:03 +0900

    Now Joint_test/inv_joint/run_ism.sh can work in ISM server

--- Joint_test/inv_joint/run_ism.sh
+++ Joint_test/inv_joint/run_ism.sh
@@ -3,13 +3,13 @@
 #source /opt/intel/oneapi/setvars.sh
 
 #PBS -q q-test
-#PBS -l select=1:ncpus=192:mem=740gb:mpiprocs=2
+#PBS -l select=1:ncpus=192:mem=740gb:mpiprocs=6
 #PBS -N TEST_JOB
 #PBS -o out.dat
 #PBS -j oe
 
 source $SELECT_PE INTEL
-export OMP_NUM_THREADS=96
+export OMP_NUM_THREADS=32
 export I_MPI_PIN_DOMAIN=omp
 export KMP_AFFINITY=compact
 cd $PBS_O_WORKDIR
@@ -24,7 +24,8 @@ make -f Makefile_ISM
 cd -
 
 echo "Start n_ebfem_joint !"
-time mpirun ${src}/ebfem_inv_joint.exe > joint.log 2>&1 <<EOF
+./clean.sh
+time mpirun ${src}/ebfem_inv_joint.exe > result_inv/joint.log 2>&1 <<EOF
 3
 active.ctl
 mt.ctl

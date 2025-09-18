dcce1eb339ca7b9a1c531c97a77b908b8c0f4086
takuto0126 <takuto.minami126@gmail.com>
2025-09-18 17:15:00 +0900

    Joint_test/inv_mt is generated

---
+++ Joint_test/inv_mt/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source /opt/intel/oneapi/setvars.sh --force
+
+src=../../src/src_inv_joint
+cd $src
+make clean
+make
+cd -
+
+export OMP_NUM_THREADS=4
+./clean.sh
+
+time mpiexec -n 6 ${src}/ebfem_inv_joint.exe <<EOF | tee result_inv/joint.log # 2025.07.31
+2
+mt.ctl
+inv_mt.ctl
+EOF
---
+++ Joint_test/inv_mt/run_ism.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#source /opt/intel/oneapi/setvars.sh
+
+#PBS -q q-test
+#PBS -l select=1:ncpus=192:mem=740gb:mpiprocs=6
+#PBS -N TEST_JOB
+#PBS -o out.dat
+#PBS -j oe
+
+source $SELECT_PE INTEL
+export OMP_NUM_THREADS=32
+export I_MPI_PIN_DOMAIN=omp
+export KMP_AFFINITY=compact
+cd $PBS_O_WORKDIR
+
+#Intel MKLモジュールのロード
+module load intelmkl/2023.2.0
+
+src=../../src/src_inv_joint
+cd $src
+#make clean -f Makefile_ISM
+make -f Makefile_ISM
+cd -
+
+# 1: only ACTIVE, 2:only MT, 3: both ACTIVE and MT
+./clean.sh
+time mpirun ${src}/ebfem_inv_joint.exe > result_inv/inv.log 2>&1 <<EOF
+2
+mt.ctl
+inv_mt.ctl
+EOF

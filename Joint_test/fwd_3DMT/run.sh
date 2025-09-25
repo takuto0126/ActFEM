b9f24e9bcf10da65ccc55c1ec4e51dda3d4377d4
takuto0126 <takuto.minami126@gmail.com>
2025-09-25 15:49:46 +0900

    Joint_test/mesh_joint/aso.ctl A_obs=0.01 and sizein=0.1

--- Joint_test/fwd_3DMT/run.sh
+++ Joint_test/fwd_3DMT/run.sh
@@ -3,15 +3,18 @@
 source /opt/intel/oneapi/setvars.sh --force
 
 ## 3D MT FWD  ##########################################################
-src=../../src/src_3DMT
+
+#src=../../src/src_3DMT    # only openMP
+src=../../src/src_3DMT_mpi # Hybrid
 cd $src
 make clean
 make
 cd -
 
-export OMP_NUM_THREADS=24
+export OMP_NUM_THREADS=4
 
-time ${src}/n_ebfem_3DMT.exe <<EOF #> result_3DMT/mt.log
+#time ${src}/n_ebfem_3DMT.exe <<EOF #> result_3DMT/mt.log
+time mpirun -np 6 ${src}/n_ebfem_3DMT_mpi.exe <<EOF #> result_3DMT/mt.log
 mt.ctl
 EOF
 
